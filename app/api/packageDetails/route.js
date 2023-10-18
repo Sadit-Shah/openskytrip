571dd7306b9132af4bfc7cf802d68c92d42d2180
Sadit Shah <saditshah@gmail.com>
2023-10-18 19:04:17 +0530

    dynamic done

--- app/api/packageDetails/route.js
+++ app/api/packageDetails/route.js
@@ -1,15 +1,14 @@
 import db from '../../lib/db';
 import Packages from '../../models/packages'
+import { revalidatePath } from 'next/cache';
 
 
-
-export async function GET(req) {
+export async function GET(request) {
     try {
         await db.connect()
         const packages = await Packages.find({});
-        console.log('Api-------------')
-        console.log(packages)
-        console.log('Api end-------------')
+        const path = request.nextUrl.searchParams.get('path')||'/'
+        revalidatePath(path)
         return new Response(JSON.stringify(packages), { status: 200 })
     } catch (error) {
         return new Response(JSON.stringify(error), { status: 500 })
--- app/pages/tours/page.js
+++ app/pages/tours/page.js
@@ -2,54 +2,32 @@ import packagescss from './tours.module.scss';
 import TourPackageCard from '../../components/tourPackageCard/tourPackageCard';
 import { BASE_API_URL } from '@/app/lib/utils/utils';
 
-//Server fetching Try..
-// export const dynamic = "force-dynamic";
-export const metadata = {
-    title: 'Tour Package Details',
-    description: 'Tour Package Details Page'
-}
 
 const getPackages = async () => {
     try {
-      const res = await fetch(`${BASE_API_URL}/api/packageDetails`, {
-        // const res = await fetch(`http://localhost:3000/api/packageDetails`, {
-        method: 'GET', // Set the HTTP method to GET
-        // headers: {
-        //   'Content-Type': 'application/json',
-        // },
-        cache: 'no-store', // Cache control, if necessary
-        // next:{
-        //     revalidate:0,
-        // }
-      });
-  
-      if (!res.ok) {
-        throw new Error('Failed to fetch packages...');
-      }
-  
-      return res.json();
+        const res = await fetch(`${BASE_API_URL}/api/packageDetails`, {
+            method: 'GET', // Set the HTTP method to GET
+            cache: 'no-store', // Cache control, if necessary
+        });
+        if (!res.ok) {
+            throw new Error('Failed to fetch packages...');
+        }
+
+        return res.json();
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  };
-  
-//---------
+}
 
 
 export default async function Tours() {
-    if(!BASE_API_URL){
+    if (!BASE_API_URL) {
         return null;
     }
-    //Server Try
     const packages = await getPackages()
-    console.log(packages)
-
-    //----------
-
     return (
         <>
-            <div className={packagescss.packagescontainer}>
-                {/* <TourPackage/> */}
+            <div className={packagescss.packagescontainer}>               
                 {packages?.map((pkg) => (
                     <TourPackageCard
                         id={pkg._id}
@@ -65,3 +43,5 @@ export default async function Tours() {
         </>
     )
 }
+
+
