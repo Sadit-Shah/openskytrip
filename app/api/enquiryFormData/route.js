5da5f8d159684b882304cad62969aebcda2598de
Sadit Shah <saditshah@gmail.com>
2023-11-12 19:11:25 +0530

    route

--- app/api/enquiryFormData/route.js
+++ app/api/enquiryFormData/route.js
@@ -28,29 +28,15 @@ const getCorsHeaders = (origin) => {
 };
 
 export async function POST(req,res) {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    req.headers.origin ? req.headers.origin : "*"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  //For CORS
-  if (req.method === "OPTIONS") {
-    res.status(200).json({result:"Ok"});
-    res.status(200).end();
-    return;
+  if(req.method == "OPTIONS"){
+    res.setHeader("Allow","POST")
+    return res.status(200).json({})
   }
-
-
+  if(req.method !== "OPTIONS"){
+    res.setHeader("Allow","POST")
+    return res.status(405).json({error: `Method ${req.method} Not Allowed`})
+  }
     await db.connect()
-
     try {
         
         const { name,
