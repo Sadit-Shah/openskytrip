8e4eaf036106c9dad8295dc0d265c693104bd29f
Sadit Shah <saditshah@gmail.com>
2023-11-13 22:58:21 +0530

    terms and cond

--- app/pages/terms_and_conditions/page.js
+++ app/pages/terms_and_conditions/page.js
@@ -12,16 +12,7 @@ export default function Termsandcond() {
                 </span>
             </div>
             <p>
-                Once a partial or full payment has been made,
-                cancellations must be submitted in writing to openskytrips@gmail.com.
-                Cancellation fees will be applied based on the date that the written
-                cancellation is received and based on the following days prior to departure.
-                Fees will be calculated as a percentage of the total amount received excluding ticketed airfare.
-                Once a partial or full payment has been made, cancellations
-                must be submitted in writing to openskytrips@gmail.com.
-                Cancellation fees will be applied based on the date that the written
-                cancellation is received and based on the following days prior to departure.
-                Fees will be calculated as a percentage of the total amount received excluding ticketed airfare.
+            Once a partial or full payment has been made, cancellations must be submitted in writing to openskytrips@gmail.com. Cancellation fees will be applied based on the date that the written cancellation is received and based on the following days prior to departure date. Fees will be calculated as a percentage of the total package amount excluding ticketed airfare.
             </p>
 
             <div className={termscss.heading}>
@@ -42,11 +33,12 @@ export default function Termsandcond() {
             </div>
             <div className={termscss.lists}>
                 <ul>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Processing fee: 13% of the total package cost will be charged regardless of any reason if you choose to cancel your trip post booking.</li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 45 days to 60 days from the trip start date: 25% of total package cost will be deducted as cancellation charge.</li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 44 to 31 days from the trip start date: 50% of total package cost will be deducted from the total package amount.</li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 30 to 21 days from the trip start date: 60% of total package cost will be deducted from the total package amount.</li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 20 and 14 days from the trip start date: 75% of total package cost will be deducted from the total package amount.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Processing fee: 13% of the total package cost will be charged regardless of any reason, if you choose to cancel your trip post booking.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 45 days to 60 days from the trip start date: 25% of total package cost.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 44 to 31 days from the trip start date: 50% of total package cost.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 30 to 21 days from the trip start date: 60% of total package cost.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Between 20 and 14 days from the trip start date: 75% of total package cost.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Anything within 14 days from the trip start date : 100% of total package cost.</li>
                     <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>In case of no-show, booking stands non-refundable.</li>
                 </ul>
             </div>
@@ -57,17 +49,16 @@ export default function Termsandcond() {
             </div>
             <div className={termscss.lists}>
                 <ul>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Pre-paid sim cards don&apos;t work in J&K due to security reasons, postpaid connection is a must.</li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>In Katra for Pithu / Palki / Pony services Municipal Committee Katra has fixed the rates and they are displayed on the rate card outside their booking office at Banganga.
-                        If someone is over charging above the listed rates it&apos;s up to you how hard you can bargain.</li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>In Srinagar if you are staying at the Houseboat: - Shikara Ride are complimentary, do not pay for the same.</li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>It is not mandatory for guest to take ponies in Gulmarg & Pahalgam to visit all the important tourist spots, all the major tourist attraction point is in close proximity so you can walk it down. However, if still you wish to take pony ride / sledging.</li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Rafting etc the rates the highly negotiable. Please bargain to your max. </li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Sightseeing to Aru Valley / Betaab Valley / Chandanwari at Pahalgam will be by local taxi union only. Due to union outside vehicles cannot enter these places. The rate is highly negotiable. </li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>If you wish to visit these places. You have to pay extra directly.   </li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>For Shopping: - Dry Fruits / Rajma / Basmati Rice / Handicraft / Woolens / Carpets/ Saffron / Cricket Bats Srinagar:- Lal Chowk / Boulevard  </li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>For Kashmiri Wazwan Food:- Aadhoos at Lal Chowk Srinagar is the best place and Mutton Kebab at Gee Enn Bakers - Residency Road.    </li>
-                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>For Vegetarian Food in Srinagar:- Krishna Dhaba is the best place. One day one destination will be covered   </li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Pre-paid sim cards don't work in J&K due to security reasons, so postpaid connection is a must.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>In Katra for Pithu / Palki / Pony services Municipal Committee Katra has fixed the rates and they are displayed on the rate card outside their booking office at Banganga. These rates are highly negotiable.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>In Srinagar, if you are staying at a Houseboat, 1 hour Shikara Ride is complimentary from OpenSkyTrips.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>For Vegetarian Food in Srinagar, Krishna Dhaba is the best place.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>It is each traveler's responsibility to have a valid government issued Identity proof.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>It is not mandatory for guest to take ponies in Gulmarg & Pahalgam to visit all the important tourist spots because most of the tourist attraction points are in close proximity & can be visited by foot.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>For any optional activities like Rafting, Snow Skiing, Snow sledging, ATV motorbiking, pony rides etc rates are highly negotiable, so we recommend to bargain with the local vendors accordingly.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Traveler is required to hire a union cab locally on direct payment basis to visit Aru, Betab valley & Chandanwari sightseeing trips in Pahalgam.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>Traveler is required to hire a union cab locally on direct payment basis to visit Fish Point, Zero Point & Thajiwas Glacier sightseeing trips in Sonamarg.</li>
+                    <li><i className="fa-solid fa-arrow-right fa-beat-fade fa-2xl"></i>In addition to all the above, if a traveler wants to go to any off route destinations that is not included in their package, additional charges to be paid by traveler locally on direct payment basis.</li>
                 </ul>
             </div>
         </div>
